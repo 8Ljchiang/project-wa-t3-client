0a616ad2546898d47b6d605df415ce666399e7c2
Jonathan C <jchiang@8thlight.com>
2019-02-18 13:22:40 -0600

    update connection api

--- src/helpers/connection.ts
+++ src/helpers/connection.ts
@@ -1,3 +1,3 @@
 // export const SERVER_URL = 'http://192.168.7.170:5000';
-export const SERVER_URL = process.env.REACT_APP_API_URI;
-export const RANDOM_MOVE_ROUTE = '/random-move';
+export const SERVER_URL = process.env.REACT_APP_T3_API_URI;
+export const RANDOM_MOVE_ROUTE = 'random-move';
